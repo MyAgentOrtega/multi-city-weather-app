225ceb5a06d7e4cf1c1d4b84ed1705d1a44cf9bd
Joey O <myagentortega@gmail.com>
2023-02-08 21:02:04 -0700

    deleted pseudo-code from JS and HTMl files

--- main.js
+++ main.js
@@ -12,25 +12,6 @@ const searchButton = document.getElementById("searchButton");
 const cityBox = document.getElementById("cityBox");
 
 searchButton.addEventListener("click", dataCompiler);
-// for (var i= document.images.length; i-->0;)
-//         document.images[i].parentNode.removeChild(document.images[i]);
-// fetch(queryURL + "&units=imperial")
-//      .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//     console.log(data);
-//     tempCard1.textContent=data.main.temp
-//     humidityCard1.textContent=data.main.humidity
-//     windCard1.textContent=data.wind.speed
-//     cityCard1.textContent=city
-//     var image = document.createElement("img");
-//     image.setAttribute("src", "https://openweathermap.org/img/wn/" + iconCode + ".png");
-//     cityCardIcon1.appendChild(image);
-//     var iconCode = data.weather[0].icon;
-
-//     })
-
 function dataCompiler() {
   console.log(cityBox.value);
   var queryURL =
